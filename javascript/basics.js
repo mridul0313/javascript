8dbe47d5ba396ecbfcf344561cbfe8fba86fa941
mridul0313 <134928012+mridul0313@users.noreply.github.com>
2024-10-05 16:30:55 +0000

    done basics

---
+++ javascript/basics.js
@@ -0,0 +1,7 @@
+const collegeId=221242
+let emailID="mridulsharma1810@gmail.com"
+var city="Varanasi"
+console.table([collegeId,emailID,city])
+// collegeId=221240
+city="Delhi"
+console.table([collegeId,emailID,city])
